65de85bb0b5586a57e260bf354dc25e51ac5566a
Quicksaver <quicksaver@gmail.com>
2014-04-16 00:00:49 +0100

    add (empty) preferences dialog

--- resource/defaults.js
+++ resource/defaults.js
@@ -1,4 +1,4 @@
-var defaultsVersion = '1.1.4';
+var defaultsVersion = '1.1.5';
 var objName = 'theFoxOnlyBetter';
 var objPathString = 'thefoxonlybetter';
 var prefList = {
@@ -20,13 +20,25 @@ function startConditions(aReason) {
 	return true;
 }
 
+function startPreferences(window) {
+	replaceObjStrings(window.document);
+	preparePreferences(window);
+}
+
 function onStartup(aReason) {
 	// Apply the add-on to every window opened and to be opened
 	windowMediator.callOnAll(startAddon, 'navigator:browser');
 	windowMediator.register(startAddon, 'domwindowopened', 'navigator:browser');
+	
+	// Apply the add-on to every preferences window opened and to be opened
+	windowMediator.callOnAll(startPreferences, null, "chrome://"+objPathString+"/content/options.xul");
+	windowMediator.register(startPreferences, 'domwindowopened', null, "chrome://"+objPathString+"/content/options.xul");
+	browserMediator.callOnAll(startPreferences, "chrome://"+objPathString+"/content/options.xul");
+	browserMediator.register(startPreferences, 'pageshow', "chrome://"+objPathString+"/content/options.xul");
 }
 
 function onShutdown(aReason) {
 	// remove the add-on from all windows
 	windowMediator.callOnAll(removeObject, null, null, true);
+	browserMediator.callOnAll(removeObject, null, true);
 }
