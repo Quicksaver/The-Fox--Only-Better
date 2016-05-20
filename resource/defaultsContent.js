f367b4a4ee0799acbc0a8ebbfbb28a3e01d08ce8
Luís Miguel <quicksaver@gmail.com>
2016-05-20 11:18:56 +0100

    updated backbone

--- resource/defaultsContent.js
+++ resource/defaultsContent.js
@@ -1,7 +1,8 @@
-// VERSION 2.0.0
+// VERSION 2.0.1
 
 // By using a JSM, we can initialize each individual tab (frame) with our scripts without having to instanciate the same objects with each one.
 (function(frame) {
-	Components.utils.import("resource://thefoxonlybetter/modules/content/utils/ModuleInSandbox.jsm");
-	ModuleInSandbox.init('thefoxonlybetter', frame);
+	let targetScope = {};
+	Components.utils.import("resource://thefoxonlybetter/modules/content/utils/ModuleInSandbox.jsm", targetScope);
+	targetScope.ModuleInSandbox.init('thefoxonlybetter', frame);
 })(this);
